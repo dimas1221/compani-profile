7575c1c6e75be2581ca9c2205f9cbb5f341fdd94
Dhimm <45517175+dimas1221@users.noreply.github.com>
2025-11-18 12:23:52 +0700

    change vit config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          router: ["react-router-dom"],
+          motion: ["framer-motion"],
+          tsparticles: ["tsparticles", "react-tsparticles"],
+        },
+      },
+    },
+  },
 });
